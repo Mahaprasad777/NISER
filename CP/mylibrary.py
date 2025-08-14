44f30fe39923c8c77129ce98ea81e6391301c7d4
Mahaprasad777 <mahaprasadm.sahoo@niser.ac.in>
2025-08-14 12:05:50 +0530

    Update mylibrary.py

--- CP/mylibrary.py
+++ CP/mylibrary.py
@@ -1,1 +1,17 @@
+import matplotlib.pyplot as plt
 
+class mylib:
+    def lcg(self,x,a,c,m):
+        l=[x]
+        for i in range(10000):
+            x=(a*x+c)%m
+            l.append(x)
+        return l
+    def Plot(self,list,k):
+        x=list[k:]
+        y=list[:-k]
+        plt.scatter(x,y)
+        plt.title(f"for k = {k}")
+        plt.xlabel("$x_i$")
+        plt.ylabel(f"$x_(i+{k})$")
+        plt.show()
